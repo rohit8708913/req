ace0af1b8af7f917bb6e71d1ab8a792337016168
rohit3607 <rohitplayer87089@gmail.com>
2024-12-11 16:31:44 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -39,8 +39,6 @@ async def is_subscribed(filter, client, update):
         return False
 
 
-# Define the filter for use in handlers
-subscribed = create(is_subscribed)
 async def encode(string):
     string_bytes = string.encode("ascii")
     base64_bytes = base64.urlsafe_b64encode(string_bytes)
