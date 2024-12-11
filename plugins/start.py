db4ddbd5aebfff94c9c0afe6beb8c944b5392d15
rohit3607 <rohitplayer87089@gmail.com>
2024-12-11 17:04:51 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -222,6 +222,8 @@ async def start_command(client: Client, message: Message):
         )
     ]
 )
+    except IndexError:
+        pass
 
         await message.reply(
             FORCE_MSG.format(
