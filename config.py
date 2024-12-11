24f78f0eefc1d930cd6844968e9e814830fc9fb3
rohit3607 <rohitplayer87089@gmail.com>
2024-12-11 20:02:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv()
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7382577497:AAE4TyePSUfPDkSJChJ4HCotcz_Z75J9F10")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7382577497:AAGUojtYjWzBeZrRUadtLq7jECHud5c1UQk")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "22469064"))
@@ -23,7 +23,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002008354608"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "7328629001"))
 
 #Port
-PORT = os.environ.get("PORT", "8021")
+PORT = os.environ.get("PORT", "8022")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://rohitplayer87089:rohit870@cluster0.4wt927p.mongodb.net/?retryWrites=true&w=majority")
