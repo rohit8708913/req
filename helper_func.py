4cf8212322fc64d1430e58e6a552ed4148e43139
rohit3607 <rohitplayer87089@gmail.com>
2024-12-11 15:30:17 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -10,21 +10,6 @@ from config import FORCE_SUB_CHANNEL, ADMINS, AUTO_DELETE_TIME, AUTO_DEL_SUCCESS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
-async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL:
-        return True
-    user_id = update.from_user.id
-    if user_id in ADMINS:
-        return True
-    try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id)
-    except UserNotParticipant:
-        return False
-
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    else:
-        return True
 
 async def encode(string):
     string_bytes = string.encode("ascii")
@@ -118,4 +103,3 @@ async def delete_file(messages, client, process):
     await process.edit_text(AUTO_DEL_SUCCESS_MSG)
 
 
-subscribed = filters.create(is_subscribed)
