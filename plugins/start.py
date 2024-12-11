48185f0800c5fac6bfd78a5322c33cd936d6024b
rohit3607 <rohitplayer87089@gmail.com>
2024-12-11 15:34:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,7 +9,7 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, START_PIC, AUTO_DELETE_TIME, AUTO_DELETE_MSG, JOIN_REQUEST_ENABLE,FORCE_SUB_CHANNEL
-from helper_func import subscribed,decode, get_messages, delete_file
+from helper_func import decode, get_messages, delete_file
 from database.database import add_user, del_user, full_userbase, present_user
 
 
@@ -298,13 +298,6 @@ async def toggle_fsub(client: Client, message: Message):
     status = "enabled" if FSUB_ENABLED else "disabled"
     await message.reply(f"Fsub has been {status}.")
 
-async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL:
-        return True
-    user_id = update.from_user.id
-    if user_id in ADMINS:
-        return True
-    try:
-        
+
 
     
