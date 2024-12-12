18cca4cdba179d2ecb5483c2ca38a58cb010e56a
rohit8708913 <koyeb67user@gmail.com>
2024-12-13 01:32:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -348,6 +348,63 @@ async def start_command(client: Client, message: Message):
 
  #=====================================================================================##
  
+
+
+#=====================================================================================##
+
+@Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
+async def get_users(client: Bot, message: Message):
+    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
+    users = await full_userbase()
+    await msg.edit(f"{len(users)} users are using this bot")
+
+@Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
+async def send_text(client: Bot, message: Message):
+    if message.reply_to_message:
+        query = await full_userbase()
+        broadcast_msg = message.reply_to_message
+        total = 0
+        successful = 0
+        blocked = 0
+        deleted = 0
+        unsuccessful = 0
+
+        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
+        for chat_id in query:
+            try:
+                await broadcast_msg.copy(chat_id)
+                successful += 1
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+                await broadcast_msg.copy(chat_id)
+                successful += 1
+            except UserIsBlocked:
+                await del_user(chat_id)
+                blocked += 1
+            except InputUserDeactivated:
+                await del_user(chat_id)
+                deleted += 1
+            except:
+                unsuccessful += 1
+                pass
+            total += 1
+
+        status = f"""<b><u>Broadcast Completed</u>
+
+Total Users: <code>{total}</code>
+Successful: <code>{successful}</code>
+Blocked Users: <code>{blocked}</code>
+Deleted Accounts: <code>{deleted}</code>
+Unsuccessful: <code>{unsuccessful}</code></b>"""
+
+        return await pls_wait.edit(status)
+
+    else:
+        msg = await message.reply(REPLY_ERROR)
+        await asyncio.sleep(8)
+        await msg.delete()
+#=====================================================================================##
+
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
     global FSUB_CHANNEL1, FSUB_CHANNEL2, FSUB_CHANNEL3, FSUB_CHANNEL4
@@ -356,22 +413,44 @@ async def not_joined(client: Client, message: Message):
     user_id = message.from_user.id
 
     try:
-        # Check if the user is subscribed to FSUB_CHANNEL1
-        sub1 = await is_subscribed1(None, client, message)
-        # Check if the user is subscribed to FSUB_CHANNEL2
-        sub2 = await is_subscribed2(None, client, message)
-        # Check if the user is subscribed to FSUB_CHANNEL3
-        sub3 = await is_subscribed3(None, client, message)
-        # Check if the user is subscribed to FSUB_CHANNEL4
-        sub4 = await is_subscribed4(None, client, message)
-
-        # If the user is subscribed to any channel
-        if sub1 or sub2 or sub3 or sub4:
+        # Check for each subscription (from channel 1 to 4)
+        if await is_subscribed1(client, message):
+            # User is subscribed to FSUB_CHANNEL1
             if not await present_user(user_id):
                 try:
                     await add_user(user_id)
                 except Exception as e:
-                    print(f"Error adding user: {e}")
+                    print(f"Error adding user to database: {e}")
+            await start_command(client, message)
+            return  # User is subscribed to at least one channel, so we return
+
+        elif await is_subscribed2(client, message):
+            # User is subscribed to FSUB_CHANNEL2
+            if not await present_user(user_id):
+                try:
+                    await add_user(user_id)
+                except Exception as e:
+                    print(f"Error adding user to database: {e}")
+            await start_command(client, message)
+            return  # User is subscribed to at least one channel, so we return
+
+        elif await is_subscribed3(client, message):
+            # User is subscribed to FSUB_CHANNEL3
+            if not await present_user(user_id):
+                try:
+                    await add_user(user_id)
+                except Exception as e:
+                    print(f"Error adding user to database: {e}")
+            await start_command(client, message)
+            return  # User is subscribed to at least one channel, so we return
+
+        elif await is_subscribed4(client, message):
+            # User is subscribed to FSUB_CHANNEL4
+            if not await present_user(user_id):
+                try:
+                    await add_user(user_id)
+                except Exception as e:
+                    print(f"Error adding user to database: {e}")
             await start_command(client, message)
             return  # User is subscribed to at least one channel, so we return
 
@@ -530,61 +609,6 @@ async def not_joined(client: Client, message: Message):
 
     except Exception as e:
         print(f"Error while checking membership: {e}")
-
-#=====================================================================================##
-
-@Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
-async def get_users(client: Bot, message: Message):
-    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
-    users = await full_userbase()
-    await msg.edit(f"{len(users)} users are using this bot")
-
-@Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
-async def send_text(client: Bot, message: Message):
-    if message.reply_to_message:
-        query = await full_userbase()
-        broadcast_msg = message.reply_to_message
-        total = 0
-        successful = 0
-        blocked = 0
-        deleted = 0
-        unsuccessful = 0
-
-        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
-        for chat_id in query:
-            try:
-                await broadcast_msg.copy(chat_id)
-                successful += 1
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-                await broadcast_msg.copy(chat_id)
-                successful += 1
-            except UserIsBlocked:
-                await del_user(chat_id)
-                blocked += 1
-            except InputUserDeactivated:
-                await del_user(chat_id)
-                deleted += 1
-            except:
-                unsuccessful += 1
-                pass
-            total += 1
-
-        status = f"""<b><u>Broadcast Completed</u>
-
-Total Users: <code>{total}</code>
-Successful: <code>{successful}</code>
-Blocked Users: <code>{blocked}</code>
-Deleted Accounts: <code>{deleted}</code>
-Unsuccessful: <code>{unsuccessful}</code></b>"""
-
-        return await pls_wait.edit(status)
-
-    else:
-        msg = await message.reply(REPLY_ERROR)
-        await asyncio.sleep(8)
-        await msg.delete()
-#=====================================================================================##
 
 #=====================================================================================##
 
