c9869dc81adb5b678c0051dc96f6211b658f74cd
rohit8708913 <koyeb67user@gmail.com>
2024-12-14 00:30:29 +0530

    Update join_reqs.py

--- plugins/join_reqs.py
+++ plugins/join_reqs.py
@@ -1,36 +1,32 @@
-from database.database import *
-from pyrogram import Client, filters, enums
+from database.database import * # Import your channel-specific databases
+from pyrogram import Client, filters
 from pyrogram.types import ChatJoinRequest
 from config import ADMINS
 
-
-#=====================================================================================##
-FSUB_CHANNEL1 = None
+# FSUB_CHANNELs - These are dynamically configurable
+FSUB_CHANNEL1 = "channel_id_1"  # Example Channel 1 ID (set dynamically as needed)
 FSUB_ENABLED1 = True
 
-FSUB_CHANNEL2 = None
+FSUB_CHANNEL2 = "channel_id_2"  # Example Channel 2 ID (set dynamically as needed)
 FSUB_ENABLED2 = True
 
-FSUB_CHANNEL3 = None
+FSUB_CHANNEL3 = "channel_id_3"  # Example Channel 3 ID (set dynamically as needed)
 FSUB_ENABLED3 = True
 
-FSUB_CHANNEL4 = None
+FSUB_CHANNEL4 = "channel_id_4"  # Example Channel 4 ID (set dynamically as needed)
 FSUB_ENABLED4 = True
 
-
-
 # This function will handle join requests for all dynamically configured FSUB_CHANNELs
-@Client.on_chat_join_request(filters.chat(lambda x: x.id in [FSUB_CHANNEL1, FSUB_CHANNEL2, FSUB_CHANNEL3, FSUB_CHANNEL4]))
+@Client.on_chat_join_request(filters.chat(lambda x: x.id in [FSUB_CHANNEL1, FSUB_CHANNEL2, FSUB_CHANNEL3, FSUB_CHANNEL4] if x.id else []))
 async def join_reqs(client, join_req: ChatJoinRequest):
-
     # Determine the channel from which the user is trying to join
-    if join_req.chat.id == FSUB_CHANNEL1:
+    if join_req.chat.id == FSUB_CHANNEL1 and FSUB_ENABLED1:
         db_instance = db1  # Use db1 for Channel 1
-    elif join_req.chat.id == FSUB_CHANNEL2:
+    elif join_req.chat.id == FSUB_CHANNEL2 and FSUB_ENABLED2:
         db_instance = db2  # Use db2 for Channel 2
-    elif join_req.chat.id == FSUB_CHANNEL3:
+    elif join_req.chat.id == FSUB_CHANNEL3 and FSUB_ENABLED3:
         db_instance = db3  # Use db3 for Channel 3
-    elif join_req.chat.id == FSUB_CHANNEL4:
+    elif join_req.chat.id == FSUB_CHANNEL4 and FSUB_ENABLED4:
         db_instance = db4  # Use db4 for Channel 4
     else:
         return  # Exit if no valid channel is specified
@@ -49,7 +45,6 @@ async def join_reqs(client, join_req: ChatJoinRequest):
             username=username,
             date=date
         )
-
 
 
 @Bot.on_message(filters.command("total1") & filters.user(ADMINS))
